cce1d38d25ee1475dd51afb93defbf7c35bf60c3
Saddem BENHAMMADI <benhammadi.saddem@gmail.com>
2025-03-15 12:36:27 +0100

    feat: implement Player class to pass test

---
+++ src/main/java/com/java/academy/Player.java
@@ -0,0 +1,19 @@
+package com.java.academy;
+
+public class Player {
+    private String name;
+    private int score;
+
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
